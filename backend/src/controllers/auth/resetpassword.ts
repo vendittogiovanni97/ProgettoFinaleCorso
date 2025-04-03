524e227b1b45609a04cafe2f75148527d6c4d8d1
vendittogiovanni97 <vendittogiovanni97@hotmail.it>
2025-04-03 16:19:54 +0200

    resetPassword

--- backend/src/controllers/auth/resetpassword.ts
+++ backend/src/controllers/auth/resetpassword.ts
@@ -1,14 +1,45 @@
-/*import { EmailManager } from '../..types/EmailManager'
-import { v4 } from 'uuid';
+import { Request, Response } from "express";
+import bcrypt from "bcrypt";
+import dbClient from "../../configuration/db.config";
+import { responseStatus } from "../../constants/status";
+import { AppSuccess } from "../../types/succesType";
+import { AppError } from "../../types/errorType";
+import { ErrorCodes } from "../../constants/errorCodes";
 
-interface ResetPasswordInfo {
-  email: string;
-  username? : string;   //meglio da email?
-  token: string;
-}
+const resetPassword = async (request: Request, response: Response) => {
+  if (!request.session.id) {
+    throw new AppError(
+      responseStatus.UNAUTHORIZED,
+      ErrorCodes.PERMISSION_DENIED,
+      "Devi essere loggato per cambiare la password"
+    );
+  }
 
+  const { newPassword } = request.body;
+  if (!newPassword || newPassword.length < 6) {
+    throw new AppError(
+      responseStatus.BAD_REQUEST,
+      ErrorCodes.INVALID_CREDENTIALS,
+      "La password deve avere almeno 6 caratteri"
+    );
+  }
 
+  // Hash della nuova password
+  const hashedPassword = await bcrypt.hash(newPassword, 10);
 
-async function resetPassword(req: Request, res: Response) {
-  const { email } = req.body;
-  const user = await*/
+  // Aggiornamento nel database
+  await dbClient.user.update({
+    where: { id: request.session.id },
+    data: { password: hashedPassword },
+  });
+  AppSuccess.getInstance().successResponse(
+    response,
+    "Password aggiornata con successo!",
+    responseStatus.OK,
+    { password: hashedPassword }
+  );
+
+  return;
+};
+
+export default resetPassword;
--- backend/src/routes/accountRouter.ts
+++ backend/src/routes/accountRouter.ts
@@ -3,17 +3,17 @@ import login from "../controllers/auth/login.contr";
 import { checkAuth } from "../middleware/isLogginMiddleware";
 import { logout } from "../controllers/auth/loggout";
 import { register } from "../controllers/auth/register.contr";
+import resetPassword from "../controllers/auth/resetpassword";
 
-
-const accountRoutes = (app:Router) => {
+const accountRoutes = (app: Router) => {
   const router = Router();
 
   router.post("/register", register);
   router.post("/login", login);
-  router.post("/logout", [checkAuth], logout );
-  router.put("/password",)
+  router.post("/logout", [checkAuth], logout);
+  router.put("/password", resetPassword);
 
-  app.use("/account", router)
-}
+  app.use("/account", router);
+};
 
-export default accountRoutes
+export default accountRoutes;
