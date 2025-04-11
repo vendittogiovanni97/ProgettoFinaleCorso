888268c24f0053647b8ef553fcf0cda20f9dbeec
eleonorab99 <eleonoratornesi@gmail.com>
2025-04-11 13:21:03 +0200

    add ngrok

--- Frontend/vite.config.ts
+++ Frontend/vite.config.ts
@@ -5,4 +5,11 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    allowedHosts: [
+      "26b5-195-94-150-225.ngrok-free.app", // Il tuo dominio Ngrok attuale
+      ".ngrok-free.app", // Permetti tutti i domini Ngrok (consigliato)
+    ],
+    host: "0.0.0.0", // Importante per accesso da rete locale
+  },
 });
